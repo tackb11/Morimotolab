6330768f1ebd7abcd445440a7eb5b130d09451f8
tackb11 <49144593+tackb11@users.noreply.github.com>
2020-01-07 16:25:57 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+setup(
+    name             = 'MorimotoLab',
+    version          = '1.0',
+    description      = 'Data-preprocessing',
+    long_description = open('README.md').read(),
+    author           = 'CHAE TACBYOUNG',
+    author_email     = 'tackb11@gmail.com',
+    url              = '',
+    download_url     = '',
+    install_requires = ['requests'],
+    packages         = find_packages(exclude = ['docs', 'example']),
+    keywords         = ['bot', 'study', 'api'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
 )
